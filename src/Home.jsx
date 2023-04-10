c807a321ab34f4039aa4b25a1d3d66cb25e69337
Paul Simbulan <pajsimbulan@gmail.com>
2023-04-10 15:44:38 -0700

    homepage done:

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
 import './Home.css';
 import logo from './assets/monogram-svg.svg';
-<i class="fa-solid fa-sun" style="color: #000000;"></i>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHands, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
     return (
         <section id="home" className="home">
-            <div className="container">  
-                <img src={logo} alt="ps logo" />
-                <div className='homeText'>
-                    <h1>Hi, I'm Paul</h1>
-                    <p>I'm a <span> Software Engineer</span></p>
-                    <p>Come see my work!</p>
+            <div className='container-outer'>
+                <div className="container-inner">  
+                    <img src={logo} alt="ps logo" />
+                    <div className='homeText'>
+                        <h1>Hi, I'm Paul
+                            <span className="handWave">  
+                                <FontAwesomeIcon icon={faHands} style={{marginLeft:'10px'}}/>
+                            </span>
+                        </h1>
+                        <p>I'm a <span style={{fontWeight:'bold'}}> Software Engineer</span></p>
+                        <p className="p1">Come see my work</p>
+                    </div>
+                </div>
+                <div className='scrollContainer'>
+                    Scroll for more
+                    <FontAwesomeIcon icon={faArrowDown} style={{margin:'auto 0 auto 10px'}}/>
                 </div>
             </div>
         </section>
