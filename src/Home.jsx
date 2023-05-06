50c9a4c00929d06dcca7dcbf4c794d1dcd42c86c
Paul Simbulan <pajsimbulan@gmail.com>
2023-05-05 18:53:20 -0700

    removed console log

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,7 +15,6 @@ function Home() {
                         </h1>
                         <p>I'm a <span style={{fontWeight:'bold'}}> Software Engineer.</span></p>
                         <p className="p1">Come get to know <span><a href="#about">me</a></span> and see my <span><a href="#projects">work</a></span> <a className="arrow" href='#about'><FontAwesomeIcon icon={faArrowDown} style={{margin:'auto 0 auto 10px', transition:'none', backgroundImage:'none'}}/></a></p>
-                        <p style={{color:'red', fontWeight:'bold'}}>**TEST RUN.    Resume, Linkedin, still to be added**</p>
                         <br />
                     </div>
                 </div>
